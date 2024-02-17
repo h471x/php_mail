46f20e393b0c4af463f29dfdf4201b6d3e9bee62
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-02-18 00:42:57 +0300

    Connection to the database

---
+++ config/connect.php
@@ -0,0 +1,24 @@
+<?php
+// Database credentials
+$host = 'localhost';
+$dbname = 'mysql';
+$username = 'emailAdmin';
+$password = 'rootemail';
+
+// Attempt to connect to the database
+try {
+  // Create a new PDO instance
+  // PHP Dtabase Object is more efficient
+  // than mysqli or mysql
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+  // Set PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // Set the message if connected successfully
+  $message = '<h1>Connected</h1>';
+} catch (PDOException $e) {
+  // Set an error message if connection fails
+  $message = "Connection failed: " . $e->getMessage();
+}
+?>
