8af9da97939833fa0118fb26a8415ecac4aeffb5
rsh <randrianirinahernandezstephan@gmail.com>
2024-03-03 22:16:59 +0300

    Deplacement de l'instanciation de PDO et utilisation de variable pour les paramtres de l'instance

--- config/php/connect.php
+++ config/php/connect.php
@@ -1,36 +1,9 @@
 <?php
 // Database credentials
-$host = 'localhost';
-$dbname = 'mail';
-$username = 'emailAdmin';
-$password = 'rootemail';
 
-// Attempt to connect to the database
-try {
-  // Create a new PDO instance
-  // PHP Dtabase Object is more efficient
-  // than mysqli or mysql
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-  // Set PDO error mode to exception
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  // Create the database if it doesn't exist
-  $pdo->exec("CREATE DATABASE IF NOT EXISTS mail");
-
-  // Select the database
-  $pdo->exec("USE mail");
-
-  // Create users table
-  $pdo->exec("CREATE TABLE IF NOT EXISTS users (
-              id INT AUTO_INCREMENT PRIMARY KEY,
-              userame VARCHAR(50) NOT NULL,
-              password VARCHAR(255) NOT NULL)");
-
-  // Set the message if connected successfully
-  $message = '<h1>Connected</h1>';
-} catch (PDOException $e) {
-  // Set an error message if connection fails
-  $message = "Connection failed: " . $e->getMessage();
-}
+// $host = 'localhost';
+// $dbname = 'mail';
+$DB_DSN = 'msql:host=localhost;dbname=mail'
+$username = 'root';
+$password = '#sudorshacker88';
 ?>
