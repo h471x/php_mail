7442b4cfd7ee0da5e8c8fb551b2ef3c374b97158
rsh <randrianirinahernandezstephan@gmail.com>
2024-03-04 22:22:54 +0300

    debogage1

--- config/php/login.php
+++ config/php/login.php
@@ -6,25 +6,26 @@ try {
     // Data from the form
     $mail = $_POST['username'];
     $password = $_POST['password'];
+    echo $mail.$password;
 
-    // Prepare SQL statement
-    $stmt = $pdo->prepare("SELECT mail FROM user WHERE password=:password");
-
-    // Bind parameters
-    // $stmt->bindParam(':username', $mail);
-    $stmt->bindParam(':password', $password);
-
-    // Execute the statement
-    $stmt->execute();
-    echo "<pre>";
-    print_r($stmt->fetch(PDO::FETCH_ASSOC))
-    echo "</pre>";
-
-    // Redirect back to index.php with success message
-    // header("Location: ../../index.php?success=true");
-    // exit; // Ensure script stops here to prevent further output
-
-    // echo "New record inserted successfully";
+    // // Prepare SQL statement
+    // $stmt = $pdo->prepare("SELECT mail FROM user WHERE password=:password");
+    //
+    // // Bind parameters
+    // // $stmt->bindParam(':username', $mail);
+    // $stmt->bindParam(':password', $password);
+    //
+    // // Execute the statement
+    // $stmt->execute();
+    // echo "<pre>";
+    // print_r($stmt->fetch(PDO::FETCH_ASSOC))
+    // echo "</pre>";
+    //
+    // // Redirect back to index.php with success message
+    // // header("Location: ../../index.php?success=true");
+    // // exit; // Ensure script stops here to prevent further output
+    //
+    // // echo "New record inserted successfully";
 } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
