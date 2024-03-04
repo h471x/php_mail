8d33c38f5615a66cd6ac1eb41d2a911cf5054b9d
rsh <randrianirinahernandezstephan@gmail.com>
2024-03-04 10:05:36 +0300

    esoriko le '

--- config/php/connect.php
+++ config/php/connect.php
@@ -3,7 +3,7 @@
 
 // $host = 'localhost';
 // $dbname = 'mail';
-$DB_DSN = "mysql:host='localhost';dbname='mail'";
+$DB_DSN = "mysql:host=localhost;dbname=mail";
 $username = 'root';
 $password = '#sudorshacker88';
 ?>
