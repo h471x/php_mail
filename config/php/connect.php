f80205243c589f6053904d663565ee7cb9c5ef5f
rsh <randrianirinahernandezstephan@gmail.com>
2024-03-04 10:00:02 +0300

    It worked ,now I'll modified this files

--- config/php/connect.php
+++ config/php/connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
 
-$host = 'localhost';
-$dbname = 'mail';
-// $DB_DSN = 'msql:host=localhost;dbname=mail'
+// $host = 'localhost';
+// $dbname = 'mail';
+$DB_DSN = "msql:host='localhost';dbname='mail'";
 $username = 'root';
 $password = '#sudorshacker88';
 ?>
