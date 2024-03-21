8719239966dbb523bff822fb29d6079f63f96651
rsh <randrianirinahernandezstephan@gmail.com>
2024-03-21 23:07:03 +0300

    trigger 1 done and work

--- triggers/insert_contact.sql
+++ triggers/insert_contact.sql
@@ -1,8 +1,11 @@
-CREATE TRIGGER insert_contact 
-AFTER INSERT 
-ON message
+
+DELIMITER //
+
+CREATE TRIGGER insert_contact AFTER INSERT ON message
 FOR EACH ROW
-BEGIN 
-  INSERT INTO contact (email_propriate, email_contact )
-  VALUES (NEW.email_user,  NEW.email_destination);
+BEGIN
+    INSERT INTO contact (Email_propriate, Email_contact) VALUES (NEW.Email_user, NEW.Email_destination);
 END;
+//
+
+DELIMITER ;
